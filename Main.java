1b64a81aaea13e380fa6af0d96caecfd48144fd7
Magooch <crvallecito@gmail.com>
2025-03-25 10:26:15 -0600

    Add atomization test function

--- Main.java
+++ Main.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Atomizer atomizer = new Atomizer();
+        Scanner scanner = new Scanner(System.in);
 
-        atomizer.testFunction();
+        System.out.println("Welcome To Our Food Atomizer and Transfer");
+
+        while (true) {
+            System.out.println();
+            System.out.print("Enter your food: ");
+            String input = scanner.nextLine();
+
+            System.out.println();
+            switch(input.toLowerCase()) {
+            case "exit":
+                System.out.println("Exiting program...");
+                System.exit(0);
+                break;
+            case "help":
+                System.out.println("An app that can atomize food down to it's molecular composition, convert that composition to transferrable data and send through telecommunication technology");
+                break;
+            default:
+                atomizer.testFunction(input); 
+            }
+        }
     }
-};
+}
